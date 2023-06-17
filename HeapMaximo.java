31bcf7363330fb6c1c01c0c5258ff08296e274c9
Diego Llerena <dllerenat@unsa.edu.pe>
2023-06-16 21:23:48 -0500

    Se agrego la funcion eliminar elemntos del Heat

--- HeapMaximo.java
+++ HeapMaximo.java
@@ -14,9 +14,8 @@ public class HeapMaximo<T extends Comparable<T>>{
     }
     public void insertElemento(T elemento){
         arreglo.add(elemento);
-        insertOrdenado(arreglo.size()-1);//devuelve el número de elmentos antes de hacer la inserción
+        insertOrdenado(arreglo.size()-1);//devuelve el número de elementos antes de hacer la inserción
     }
-
     private void swap(int i, int j) {
         T temp = arreglo.get(i);
         arreglo.set(i, arreglo.get(j));
@@ -25,14 +24,37 @@ public class HeapMaximo<T extends Comparable<T>>{
     public void insertOrdenado(int index){
         int ParentIndex =(index-1)/2; //Sirve para hallar la ubicación del padre del elemento
         //Si el index es mayor que 0 es la raíz no se debe cambiar posiciones
-        while(index > 0 && arreglo.get(index).compareTo(arreglo.get(ParentIndex))>0){
+        while(index > 0 && arreglo.get(index).compareTo(arreglo.get(ParentIndex))>0){ //Si el hijo es mayor que el padre deben cambiar de lugar para que sea un arbol máximo.
             swap(index, ParentIndex);//Intercambia el padre con el hijo, para recorrer el arbol.
             index = ParentIndex;
             ParentIndex= (index - 1) / 2;
         }
     }
-    public void eliminar(ArrayList<T> arreglo ) {
-       
+    public T eliminar() {
+       if (isEmpty()) {
+            throw new IllegalStateException("Arreglo vacio");
+        }
+        T max = arreglo.get(0);
+        int lastIndex = arreglo.size() - 1;
+        arreglo.set(0, arreglo.get(lastIndex));//reemplaza el menor de todos los elementos por la raiz para reconstruir el Heap
+        arreglo.remove(lastIndex);//se elimina el elemento removido del arreglo
+        eliminar(0);// en este arreglo siempre se elimina el mayor de todos lo elementos
+        return max;
+    }
+    private void eliminar(int index) {
+        int hijoIzquierdo = 2 * index + 1;
+        int hijoDerecho = 2 * index + 2;
+        int largestIndex = index;
 
+        if (hijoIzquierdo< arreglo.size() && arreglo.get(hijoIzquierdo).compareTo(arreglo.get(largestIndex)) > 0) {//Si el hijo izquierdo es mayor se intercambia
+            largestIndex = hijoIzquierdo;
+        }
+        if (hijoDerecho< arreglo.size() && arreglo.get(hijoDerecho).compareTo(arreglo.get(largestIndex)) > 0) {//Si el hijo derecho es mayor se intercambia
+            largestIndex = hijoDerecho;
+        }
+        if (largestIndex != index) {
+            swap(index, largestIndex);//Se intercambian los valores para ordenar el arbol
+            eliminar(largestIndex);//se continua evaluando desde la posición que se cambio
+        }
     }
 }
--- Test.java
+++ Test.java
@@ -1,14 +1,23 @@
 public class Test {
     public static void main(String[]args){
         HeapMaximo<Integer> arbol= new HeapMaximo<>();
-        arbol.insertElemento(12);
+        arbol.insertElemento(14);
+        arbol.insertElemento(8);
+        arbol.insertElemento(2);
         arbol.insertElemento(10);
-        arbol.insertElemento(13);
-        arbol.insertElemento(15);
-        arbol.insertElemento(16);
+        arbol.insertElemento(9);
+        arbol.insertElemento(7);
+        arbol.insertElemento(4);
+        arbol.insertElemento(3);
+        arbol.insertElemento(1);
+        arbol.insertElemento(16); 
         for (Integer p : arbol.getArreglo()) {
-            System.out.println(p);
-        
+            System.out.print(p+", ");
+        }
+        System.out.println();
+        arbol.eliminar();
+        for (Integer p : arbol.getArreglo()) {
+            System.out.print(p+ ", ");
         }
     }
 }
